bc0c2468ef2e22372fe77c9410c65e553cf6a3a2
pingvinya <coepop31@gmail.com>
2023-08-31 00:31:15 +0300

    improved commands, terminal

--- swan_os/src/terminal.rs
+++ swan_os/src/terminal.rs
@@ -2,6 +2,9 @@ use swan_kernel::{task::keyboard, println, print};
 use pc_keyboard::{layouts, DecodedKey, HandleControl, Keyboard, ScancodeSet1};
 use futures_util::stream::StreamExt;
 use alloc::string::String;
+use crate::alloc::string::ToString;
+use alloc::vec::Vec;
+use hashbrown::HashMap;
 use swan_kernel::vga_buffer::*;
 
 const CONFIG: (u8, Color) = (b'>', Color::Yellow);
@@ -39,9 +42,53 @@ pub async fn run() {
     }
 }
 
-fn parse(buffer: String) {
-    match buffer.as_str() {
-        "test" => println!("test command"),
-        _ => println!("command '{}' not found.", buffer)
+pub struct Command {
+    command: String,
+    args: Vec<String>
+}
+
+impl Command {
+
+    pub fn new(command: &str) -> Self {
+        Self {
+            command: command.to_string(),
+            args: Vec::new()
+        }
     }
+
+    pub fn arg(&mut self, arg: &str) -> &Self {
+        self.args.push(arg.to_string());
+        self
+    }
+
+    pub fn build(&self, buffer: String, f: fn(HashMap<String, String>)) {
+        let mut args = buffer.split_whitespace().collect::<Vec<&str>>();
+        let command = args.remove(0).to_string();
+
+        if command == self.command {
+
+            assert_eq!(self.args.len(),args.len());
+
+            let mut parsed_args = HashMap::new();
+
+            for arg in 0..self.args.len() {
+                parsed_args.insert(
+                    self.args[arg].clone(),
+                    args[arg].to_string()
+                );
+            }
+
+            f(parsed_args);
+        }
+    }
+
+}
+
+fn parse(buffer: String) {
+    Command::new("test")
+        .arg("test_arg")
+        .build(buffer.clone(), |args| println!("{}", args["test_arg"]));
+
+    Command::new("help")
+        .build(buffer.clone(), |args| println!("kek2"));
 }
